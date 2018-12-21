4f8420d879fa19cb52bf8868eac5b3f112a07089
wdzp <43175481+wdzp@users.noreply.github.com>
2018-12-21 10:03:25 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Lesson.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace App1
+{
+    [Serializable]
+    public class Lesson
+    {
+        public string LessonName { get; set; }
+        public string Day { get; set; }
+        public string BeginWeek { get; set; }
+        public string EndWeek { get; set; }
+        public string BeginTime { get; set; }
+        public string EndTime { get; set; }
+        public string Place { get; set; }
+        public string Classroom { get; set; }
+        public string WeekInterVal { get; set; }
+        public string TeacherName { get; set; }
+        public string ProfessionName { get; set; }
+        public string Credit { get; set; }
+    }
+    class xml
+    {
+        public void serializeMethod(List<Lesson> list)
+        {
+            using (FileStream fs = new FileStream("s.xml", FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, list);
+            }
+        }
+        public List<Lesson> reserializeMethod(string fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                List<Lesson> list = (List<Lesson>)bf.Deserialize(fs);
+                return list;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Lesson> lessons;
+            xml xml = new xml();
+            lessons = xml.reserializeMethod("s.xml");
+        }
+    }
+}
